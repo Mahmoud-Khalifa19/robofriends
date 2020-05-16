b57ebef492c6bdc2fc792644327e8b52843f544e
mahmoud khalifa <mahmoudbkhalifa@gmail.com>
2020-05-16 22:29:29 +0100

    Updates

---
+++ precache-manifest.2521f22ade3ee0b3c77d9f349fe3c7a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49cdcf1633466289665da97cc10ac15",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ff15adf7f820f7d1c17e",
+    "url": "/robofriends/static/css/main.b33a8a4b.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff15adf7f820f7d1c17e",
+    "url": "/robofriends/static/js/main.42d70d40.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
